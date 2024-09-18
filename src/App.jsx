7b39cc6c41faee320b93ee77ef0a0c43cedc59d2
ALI ASGAR <aliasgar.mithaiwala2022@vitstudent.ac.in>
2024-09-18 11:40:42 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 
+
 const App = () => {
   return (
     <div>
+      
       <Navbar/>
+     
+     
   
 
       
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+// Create the root of the application
+const root = createRoot(document.getElementById('root'));
 
-
-createRoot(document.getElementById('root')).render(
-  <Router>
-  <App />
-</Router>,
-document.getElementById('root')
-)
+root.render(
+  <StrictMode>
+    {/* Wrap the app with Auth0Provider and Router */}
+    <Auth0Provider
+      domain="dev-kjno4tk8yj3wbz0c.us.auth0.com"
+      clientId="Lag4MmFB3bsRQqZx0AYfvUcOJTKfj26T"
+      authorizationParams={{ redirect_uri: window.location.origin }}
+    >
+      <Router>
+        <App />
+      </Router>
+    </Auth0Provider>
+  </StrictMode>
+);
