df1d8a0a5cb5aa6e1c663e87f802d3b118d01a57
ALI ASGAR <aliasgar.mithaiwala2022@vitstudent.ac.in>
2024-09-18 11:17:26 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+  
+
+      
+    </div>
+  )
+}
+
+export default App
